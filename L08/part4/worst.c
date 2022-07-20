d204b9e0a91bfd31bd3cd017b099a63dfe588429
shuyu <shuyuliu1949@gmail.com>
2022-07-20 17:08:40 -0400

    wew

--- L08/part4/worst.c
+++ L08/part4/worst.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "library.h"
+#include <stdbool.h>
 
 int main()
 {
 
     int array_1[4] = {1, 2, 3, 4};
     printf("%d", array_1[5]);
-  
+    bool x = false;
+    x++;
+    if (false){
+        printf("hello error");
+    }else if (false)(
+        printf("hello error");
+    )
+    long int y = 10/0;
     return 0;
 }
