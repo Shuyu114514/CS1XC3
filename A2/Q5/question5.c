307093d550347cb6544cddb300ad39685596a44d
liu shuyu <shuyuliu1949@gmail.com>
2022-08-04 07:40:11 -0400

    finish!

--- A2/Q5/question5.c
+++ A2/Q5/question5.c
@@ -1,8 +1,48 @@
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
+#include <ctype.h>
 
-float avgCSV (char* inputName);
+float avgCSV (char* inputName){
+	float a = 0;
+	char s_a[100];
+	int num_numbers = 0;
+	float sum = 0;
+	char temp_line[200];
+	int i = 0;
+	int position = 0;
+	int num_rows = 0;
+
+	FILE *fp = fopen(inputName, "r");
+	while(fscanf(fp, "%[^\n] ", temp_line) != EOF){
+		//printf("\ngot: %s\n", temp_line);
+		num_rows = 0;
+		i = 0;
+		while(temp_line[i] != '\0' && num_rows < 8){
+			if(isdigit(temp_line[i]) || temp_line[i] == '.'){
+				while(isdigit(temp_line[i]) || temp_line[i] == '.'){
+					s_a[position] = temp_line[i];
+					position+=1;
+					i+=1;
+				}
+				s_a[position] = '\0';
+				a = atof(s_a);
+				sum+=a;
+				num_numbers+=1;
+				num_rows+=1;
+				position = 0;
+			}
+			i+=1;
+		}
+	}
+	fclose(fp);
+	//printf("num: %d sum: %f\n", num_numbers, sum);
+	if(num_numbers != 0){
+		return sum/num_numbers;
+	}else{
+		return 0;
+	}
+}
 
 int main () {
 	printf("%f\n", avgCSV("multiline1.csv"));
--- A2/Q6/question6.c
+++ A2/Q6/question6.c
@@ -1,15 +1,71 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <assert.h>
 
 struct node {
-// Your code here
+    char *name;
+	char *tea;
+    struct node *left;
+    struct node *right;
 };
 
 typedef struct node Node;
 
-int depth(struct node *root);
-int dfs(struct node *root, char *tok);
+int depth(struct node *root){
+    if (root == NULL) {
+		return 0;
+	}
+	else{
+		int LeftDepth = depth(root->left);
+		int RightDepth = depth(root->right);
+        if(LeftDepth > RightDepth){
+            return LeftDepth+1;
+        }else{
+            return RightDepth+1;
+        }
+	}
+}
+int count_sub(struct node *root){
+    if(root->left != NULL && root->right != NULL){
+       return 1+count_sub(root->left)+count_sub(root->right);
+    }else if(root->left != NULL && root->right == NULL){
+        return 1+count_sub(root->left);
+    }else if(root->left == NULL && root->right != NULL){
+        return 1+count_sub(root->right);
+    }else{
+        return 1;
+    }
+}
+int dfs(struct node *root, char *tok){
+    int a = -1;
+    int b = -1;
+    //node *temp = malloc(sizeof(node));
+    if (root == NULL){
+		return -1;
+	}else if(strcmp(root->name, tok) == 0){
+        return 1;
+    }
+
+    if(root->left != NULL){
+       a = dfs(root->left, tok); 
+    }
+    if(a > 0){
+        return 1+a;
+    }
+    if(root->right != NULL){
+        b = dfs(root->right, tok); 
+    }
+    if(b > 0 && root->left != NULL){
+        return 1+b+count_sub(root->left);
+    }else if(b > 0){
+        return 1+b;
+    }
+    return -1;
+    
+}
 
 /******************The binary tree***********************
  * The following is an illustration of the binary tree
@@ -72,6 +128,8 @@ int main() {
     
     root->right->left->right->left->name = "Bassel";
     root->right->left->right->left->tea = "Matcha";
+
+    printf("The sub tree of Parker: %d\n", count_sub(root->right));
     
     printf("The depth of the tree is: %d\n", depth(root));
     // expected result is 5.
@@ -92,6 +150,9 @@ int main() {
     
     printf("Depth First Search for : Swaleh\n");
     printf("result: %d\n", dfs(root, "Swaleh"));
+
+    printf("Depth First Search for : Mark\n");
+    printf("result: %d\n", dfs(root, "Mark"));
     
     printf("Depth First Search for : Ana\n");
     printf("result: %d\n", dfs(root, "Ana"));
